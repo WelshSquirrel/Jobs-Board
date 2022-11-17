a792c2015bce1f784a90a07590447b1592a7d414
WelshSquirrel <109298669+WelshSquirrel@users.noreply.github.com>
2022-11-17 16:04:10 +0000

    initial commit

---
+++ src/Register.js
@@ -0,0 +1,51 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PASS_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pass, setPass] = useState('');
+    const [validPass, setValidPass] = useState(false);
+    const [passFocus, setPassFocus] = useState(false);
+
+    const [matchPass, setMatchPass] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setValidName(USER_REGEX.test(user));
+    }, [user])
+
+    useEffect(() => {
+        setValidPwd(PASS_REGEX.test(pass));
+        setValidMatch(pass === matchPass);
+    }, [pass, matchPass])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pass, matchPass])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Register;
